8d73306d3253589b5e08cde86de49a9b112cba48
Pakorn Jampoka <pakorn.jampoka@thoughtworks.com>
2022-05-24 09:44:12 +0700

    Add react components for product list and theme changing

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import { Product } from '../../app/models/product';
+import ProductList from './ProductList';
+
+function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch('https://localhost:7139/api/Products');
+      let data = await response.json();
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
+}
+
+export default Catalog;
